1472c727aabf4a3fcffa130a06321b5b70ecd29e
oluwoleolusi <oluwole.olusi@gmail.com>
2022-08-04 01:30:45 +0100

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,66 @@
+import dynamic from "next/dynamic";
+import PropTypes from "prop-types";
+const Navigation = dynamic(() => import("../components/Navigation"));
+const Greetings = dynamic(() => import("../containers/Greetings"));
+const Skills = dynamic(() => import("../containers/Skills"));
+const Proficiency = dynamic(() => import("../containers/Proficiency"));
+const Education = dynamic(() => import("../containers/Education"));
+const Experience = dynamic(() => import("../containers/Experience"));
+const Projects = dynamic(() => import("../containers/Projects"));
+const Feedbacks = dynamic(() => import("../containers/Feedbacks"));
+const GithubProfileCard = dynamic(() =>
+	import("../components/GithubProfileCard")
+);
+import { openSource } from "../portfolio";
+import SEO from "../components/SEO";
+
+export default function Home({ githubProfileData }) {
+	return (
+		<div>
+			<SEO
+				data={{
+					title: "Oluwole Olusi",
+					description:
+						"A passionate full stack developer and graphic designer",
+					image: "https://avatars3.githubusercontent.com/u/59178380?v=4",
+					url: "https://developer-portfolio-1hanzla100.vercel.app",
+					keywords: [
+						"Oluwole Olusi",
+						"Web Developer",
+						"React Developer",
+						"CSS",
+						"HTML",
+						"Javascript",
+						"Flask",
+						"API",
+						"Flask-SQLAlchemy",
+						"Python",
+					],
+				}}
+			/>
+			<Navigation />
+			<Greetings />
+			<Skills />
+			<Proficiency />
+			<Education />
+			<Experience />
+			{/* <Feedbacks /> */}
+			<Projects />
+			<GithubProfileCard prof={githubProfileData} />
+		</div>
+	);
+}
+
+Home.prototype = {
+	githubProfileData: PropTypes.object.isRequired,
+};
+
+export async function getStaticProps(_) {
+	const githubProfileData = await fetch(
+		`https://api.github.com/users/${openSource.githubUserName}`
+	).then((res) => res.json());
+
+	return {
+		props: { githubProfileData },
+	};
+}
---
+++ portfolio.js
@@ -0,0 +1,292 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+	name: "Oluwole Olusi",
+	title: "Hi, I'm Wole",
+	description:
+		"I am Oluwole Olusi, a full stack developer and graphic designer. I work with React, and integrate backend functionality using PHP/MySQL or Python's Flask SQLAlchemy library. I love to create new things. Let's create something amazing.",
+		resumeLink:
+		"https://drive.google.com/file/d/16RVIttw9OhhU1KvQ6NsnXJQhNOglLDLv/view?usp=sharing",
+};
+
+export const openSource = {
+	githubUserName: "oluwoleolusi",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+	facebook: "https://www.facebook.com/oluwolebabatundeolusi/",
+	// instagram: "https://www.instagram.com/1hanzla100",
+	twitter: "https://twitter.com/wolei_sway",
+	github: "https://github.com/oluwoleolusi",
+	// linkedin: "https://www.linkedin.com/in/1hanzla100/",
+};
+
+
+
+export const skillsSection = {
+	title: "What I Do",
+	subTitle:
+		"Full Stack Developer and Experienced Designer",
+	data: [
+		{
+			title: "Full Stack Development",
+			lottieAnimationFile: "/lottie/skills/fullstack.json", // Path of Lottie Animation JSON File
+			skills: [
+				emoji(
+					"⚡ Building responsive web applications in React.js"
+				),
+				emoji("⚡ Building responsive static websites using React.js"),
+				emoji(
+					"⚡ Building RESTful APIs in Flask"
+				),
+			],
+			softwareSkills: [
+				{
+					skillName: "HTML-5",
+					fontAwesomeClassname: "vscode-icons:file-type-html",
+				},
+				{
+					skillName: "CSS-3",
+					fontAwesomeClassname: "vscode-icons:file-type-css",
+				},
+				{
+					skillName: "JavaScript",
+					fontAwesomeClassname: "logos:javascript",
+				},
+				{
+					skillName: "Reactjs",
+					fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+				},
+				{
+					skillName: "Python",
+					fontAwesomeClassname: "logos:python",
+				},
+				{
+					skillName: "PHP",
+					fontAwesomeClassname: "logos:php",
+				},
+				{
+					skillName: "mySQL",
+					fontAwesomeClassname: "logos:mysql",
+				},
+				{
+					skillName: "Postgresql",
+					fontAwesomeClassname: "logos:postgresql",
+				},
+				{
+					skillName: "Flask",
+					fontAwesomeClassname: "logos:flask",
+				},
+				{
+					skillName: "JQuery",
+					fontAwesomeClassname: "logos:jquery",
+				}
+			],
+		}
+	],
+};
+
+export const SkillBars = [
+	{
+		Stack: "HTML5", //Insert stack or technology you have experience in
+		progressPercentage: "90", //Insert relative proficiency in percentage
+	},
+	{
+		Stack: "CSS3",
+		progressPercentage: "80",
+	},
+	{
+		Stack: "Javascript",
+		progressPercentage: "80",
+	},
+	{
+		Stack: "PHP/MYSQL",
+		progressPercentage: "75",
+	},
+	{
+		Stack: "Python",
+		progressPercentage: "70",
+	},
+	{
+		Stack: "Flask SQLAlchemy",
+		progressPercentage: "85",
+	},
+	{
+		Stack: "Postgresql",
+		progressPercentage: "80",
+	},
+	{
+		Stack: "React",
+		progressPercentage: "80",
+	},
+	{
+		Stack: "JQuery",
+		progressPercentage: "90",
+	}
+];
+
+export const educationInfo = [
+	{
+		schoolName: "University of Ibadan",
+		subHeader: "Bachelor of Science in Economics",
+		duration: "December 2011 - December 2015",
+		// desc: "",
+		// descBullets: [
+		// 	"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		// ],
+	},
+	{
+		schoolName: "W3Schools",
+		subHeader: "HTML5 Certification",
+		// duration: "December 2011 - December 2015",
+		// desc: "",
+		// descBullets: [
+		// 	"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		// ],
+	},
+	{
+		schoolName: "W3Schools",
+		subHeader: "CSS3 Certification",
+		// duration: "December 2011 - December 2015",
+		// desc: "",
+		// descBullets: [
+		// 	"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		// ],
+	},
+	{
+		schoolName: "W3Schools",
+		subHeader: "Javascript Certification",
+		// duration: "December 2011 - December 2015",
+		// desc: "",
+		// descBullets: [
+		// 	"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		// ],
+	},
+	{
+		schoolName: "Udemy",
+		subHeader: "Complete UI/UX Design Course",
+		// duration: "December 2011 - December 2015",
+		// desc: "",
+		// descBullets: [
+		// 	"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		// ],
+	},
+	{
+		schoolName: "Free Code Camp",
+		subHeader: "PHP Programming Language Tutorial – Full Course",
+		// duration: "December 2011 - December 2015",
+		// desc: "",
+		// descBullets: [
+		// 	"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		// ],
+	},
+	{
+		schoolName: "Free Code Camp",
+		subHeader: "MySQL Tutorial – Full Database Course",
+		// duration: "December 2011 - December 2015",
+		// desc: "",
+		// descBullets: [
+		// 	"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		// ],
+	},
+	{
+		schoolName: "Udemy",
+		subHeader: "JQuery Full Course",
+		// duration: "December 2011 - December 2015",
+		// desc: "",
+		// descBullets: [
+		// 	"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		// ],
+	},
+	{
+		schoolName: "Coursera",
+		subHeader: "Full React Digest",
+		// duration: "December 2011 - December 2015",
+		// desc: "",
+		// descBullets: [
+		// 	"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		// ],
+	},
+	{
+		schoolName: "Udacity",
+		subHeader: "Full Stack Developer",
+		// duration: "December 2011 - December 2015",
+		// desc: "",
+		// descBullets: [
+		// 	"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		// ],
+	}
+];
+
+export const experience = [
+	{
+		role: "Tech Support Lead",
+		company: "Shindara Co",
+		companylogo: "/img/shindara_logo.png",
+		date: "May 2021 – Feb 2022",
+		desc: "I worked as a Graphic Designer, and Full Stack Developer for an online fashion store.",
+		descBullets: [
+			"Produced all marketing design content used to promote products, events etc.",
+			"Provided all design assets used on the website.",
+			"Designed and developed the company website within the Shopify framework.",
+			"Edited product photos and photos from photoshoots used on banners and marketing communication.",
+			"Produced all marketing design content used to promote products, events etc.",
+			"Provided all design assets used on the website.",
+			"Designed and developed the company website within the Shopify framework.",
+			"Edited product photos and photos from photoshoots used on banners and marketing communication.",
+		],
+	},
+	{
+		role: "Graphic Designer & Content Creator",
+		company: "JeJe LLC",
+		companylogo: "/img/jeje_logo.png",
+		date: "Jan 2017 - June 2018",
+		desc: "Worked remotely as a graphic designer, and I was in charge of their social media.",
+		descBullets: [
+			"Drafted and executed a new identity for the brand, consistent across various media.",
+			"Conceptualized and created all designs related to the company’s day-to-day business.",
+			"Produced all marketing design content used to promote products, events etc.",
+			"Provided all design assets needed by the resident web developer.",
+			"Supervised and advised the social media manager regarding design-related issues and copy writing.",
+			"Catalogued and wrote detailed product descriptions for every single product in the company’s line.",
+		]
+	},
+];
+
+export const projects = [
+	{
+		name: "Shindara Co",
+		desc: "An online fashion store, built using a mix of Javascript, HTML/CSS, and Shopify's Liquid",
+		link: "https://shindara.co",
+	},
+	{
+		name: "Design Portfolio",
+		desc: "You can check out my design portfolio on Behance",
+		des_link: "https://behance.net/oluwoleolusi",
+	}
+];
+
+export const feedbacks = [
+	{
+		name: "John Smith",
+		feedback:
+			"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.",
+	},
+	{
+		name: "John Smith",
+		feedback:
+			"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.",
+	},
+];
